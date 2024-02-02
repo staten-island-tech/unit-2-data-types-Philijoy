60bbcb7cd2da6707e558df4eb5423eadb0a81a15
Philijoy <158211486+Philijoy@users.noreply.github.com>
2024-02-02 13:58:55 -0500

    :)

---
+++ data.py
@@ -0,0 +1,21 @@
+#Data types, Numbers 1,2,3 etc.
+
+b = 2
+y = float(4)
+print(b,y)
+
+t = 3
+y = float(4)
+print(t,y)
+
+p = 4
+y = float(4)
+print(p,y)
+
+
+#Lists
+values = [1,2.23,26,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
+
