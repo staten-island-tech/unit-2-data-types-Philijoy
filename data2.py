519ddc0c23f44f467c09beef6dee56ebe9cdcd94
Philijoy <158211486+Philijoy@users.noreply.github.com>
2024-02-06 13:58:14 -0500

    .

---
+++ data2.py
@@ -0,0 +1,9 @@
+service = x 
+if service == 0:
+    print("This service is bad")
+elif service == (15):
+    print("The service is okay")
+elif service == (20):
+    print("The service is good")
+else: (25)
+print("The service is great")
