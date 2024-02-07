2ecefaeac0a74f215803f5cc2f9bfd2bcf5797ff
Philijoy <158211486+Philijoy@users.noreply.github.com>
2024-02-07 13:19:37 -0500

    7

---
+++ data1.py
@@ -0,0 +1,64 @@
+#booleans
+x = False
+
+def check(x):
+    if (x== True):
+        print("is logged in")
+    else:
+        print("please log in")
+check(x)
+
+
+def add(x,y):
+    print(x + y)
+add(45,35)
+
+#booleans and control
+day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect")
+
+
+#Strings "a,b,c"
+def message(name):
+    print(name)
+message("meow")
+x = "1"
+int(x)
+
+#strings and data is different
+add("35","45")
+
+
+#challenge odd and even
+x = input("give number") (x%2)
+if (int (x)%2) == 0:
+    print("even")
+else:
+    print("odd")
+#odd and even
+print(4350%2)
+
+
+#bill and tip
+int()
+float()
+
+service = x 
+if service == 0:
+    print("This service is bad")
+elif service == (15):
+    print("The service is okay")
+elif service == (20):
+    print("The service is good")
+else: (25)
+print("The service is great")
+
+
+
+
+
+
+
--- data2.py
+++ data2.py
@@ -7,3 +7,13 @@ elif service == (20):
     print("The service is good")
 else: (25)
 print("The service is great")
+
+
+# tip calculator 
+
+bill = float(input("Enter your bill:"))
+tip = int(input("Enter your tip:"))
+
+print("bill = ", bill)
+print("tip = ", tip)
+print("total = ", bill + tip)
