36dab55901099d5c000b3be95f8d26c175c749dc
Philijoy <158211486+Philijoy@users.noreply.github.com>
2024-02-07 13:57:55 -0500

    :))))

--- data.py
+++ data.py
@@ -1,23 +1,24 @@
 #Data types, Numbers 1,2,3 etc.
 
+#bill
 b = 2
 y = float(4)
 print(b,y)
+
+#tip
+t = 3
+y = float(4)
+print(t,y)
+
+#total
+p = 4
+y = float(4)
+print(p,y)
+
 
 #Lists
 values = [1,2.23,26,7,2,30,15]
 print(values)
 for i in values:
     print(i)
-
-#Print a certain value
-print(values[1])
-print(values[2])
-
-# string methods
-x = "this is a thing"
-y= x.split( )
-z = y[0]
-print(y)
-print(z)
 
--- data2.py
+++ data2.py
@@ -1,4 +1,24 @@
-service = x 
+# tip calculator 
+
+bill = float(input("Enter your bill:"))
+tip = int(input("Enter your tip:"))
+
+print("bill = ", bill)
+print("tip = ", tip)
+print("total = ", bill + tip)
+
+#challenge odd and even
+x = input("give number") (x%2)
+if (int (x)%2) == 0:
+    print("even")
+else:
+    print("odd")
+#odd and even
+print(4350%2)
+
+
+# challenge: service percent code
+service = x
 if service == 0:
     print("This service is bad")
 elif service == (15):
@@ -9,11 +29,3 @@ else: (25)
 print("The service is great")
 
 
-# tip calculator 
-
-bill = float(input("Enter your bill:"))
-tip = int(input("Enter your tip:"))
-
-print("bill = ", bill)
-print("tip = ", tip)
-print("total = ", bill + tip)
