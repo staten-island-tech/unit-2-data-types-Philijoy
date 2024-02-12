6416c8509c8f80a0d78081e4a2e3e64e5bc8a848
Philijoy <158211486+Philijoy@users.noreply.github.com>
2024-02-11 21:02:17 -0500

    y

--- data3.py
+++ data3.py
@@ -1,8 +1,12 @@
-def all_factors(x): 
-    print("The factors are: ")
+factor = int(input ("Give number "))
+factor1 = int(input ("Give number "))
+
+def greatest_factor(x): 
     for i in range(1, x + 1):
         if x % i == 0:
             print(i)
-
-number = 125
-all_factors(number)
+values = greatest_factor(factor)
+print('Factoring next #')
+values1 = greatest_factor(factor1)
+Gcf = [values, values1] 
+print(Gcf)
