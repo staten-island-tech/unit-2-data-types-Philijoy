78b8c6839d07661846887b6d20567bcffebb669e
Philijoy <158211486+Philijoy@users.noreply.github.com>
2024-02-26 13:50:37 -0500

    :)))

--- data3.py
+++ data3.py
@@ -1,19 +1,11 @@
+def calculate_gcf(x,y):
+    while y!=0:
+         z=y
+         y=int(x)%int(x)
+         x=z
+         return z
 
-
-number1 = int(input("Pick a number: "))
-number2 = int(input("Pick another number: "))
-
-def GCF(number1,number2):
-    if number1 < number2:
-        smaller_number = number1
-    else:
-        smaller_number = number2
-
-    for i in range(1, smaller_number + 1):
-        if (number1 % i == 0) and (number2 % i == 0):
-          gcf = i
-
-    return gcf
-
-print("The Greatest common factor of number 1 and 2 is: ", GCF(number1, number2))
-
+num1 = int(input("give a number: "))
+num2 = int(input("give another number: "))
+gcf = calculate_gcf (1, 2)
+print("The greatest common factor of number 1 and 2 is: ")
